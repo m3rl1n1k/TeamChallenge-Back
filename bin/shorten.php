83d873caa4f257ac70ab2f5ea8f8dac532988525
Fenix-25 <serqdrago@gmail.com>
2023-03-28 18:30:23 +0300

    ver_1.0.1

--- bin/shorten.php
+++ bin/shorten.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__ . "/../vendor/autoload.php";
 use Controllers\UrlShortener;
 
 $filePath = __DIR__ . "/../storage/urls.json";
@@ -7,10 +8,8 @@ $filePath = __DIR__ . "/../storage/urls.json";
 $short = new UrlShortener($filePath);
 $short->setLength(10);
 
-$codeShort = $short->encode("https://free-url-shortener.rb.gy/1111");
-$url = $short->decode("3a7e0e33b0");
-echo PHP_EOL;
-echo $short->encode("https://free-url-shortener.rb.gy/1111");
+$codeShort = $short->encode("https://google.com/page-1");
+$url = $short->decode("3a8ba2c470");
 echo PHP_EOL;
 echo $url;
 echo PHP_EOL;
--- src/Controllers/Validation.php
+++ src/Controllers/Validation.php
@@ -6,6 +6,8 @@ class Validation
 {
 	public function isValidUrl(string $url): bool
 	{
-		return filter_var($url, FILTER_VALIDATE_URL) !== false;
+		// прротокол + доменна назва . домен : порт(якщо існує)/ назва каталогу
+		$pattern = '/^https?:\/\/[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})(:[0-9]{1,5})?(\/.*)?$/i';
+		return preg_match($pattern, $url);
 	}
 }
