4b7a29ae760ec6c7eebd6fa79bac33088460c05d
Fenix-25 <serqdrago@gmail.com>
2023-05-27 02:04:39 +0300

    допрацювання

--- public/index.php
+++ public/index.php
@@ -1,2 +1,29 @@
 <?php
-phpinfo();
+
+use Bisix21\src\Classes\Divider;
+use Bisix21\src\Core\Config;
+use Bisix21\src\Core\DI\Container;
+use Bisix21\src\Core\Handler;
+use Monolog\Logger;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+const ROOT = __DIR__ . "/../";
+require_once ROOT . "src/bootstrap.php";
+$services = Config::instance()->get("services");
+try {
+	/**
+	 * @var Logger $monolog
+	 **/
+	Container::getInstance($services);
+	$monolog = Container::getInstance()->get(Logger::class);
+	$handle = Container::getInstance()->get(Handler::class);
+	/** @var Handler $handle */
+	$handle->handle();
+} catch (NotFoundExceptionInterface|ContainerExceptionInterface $e) {
+	Divider::printString($e->getMessage() . $e->getFile());
+	$monolog->error($e->getMessage() . $e->getFile());
+} catch (InvalidArgumentException $argumentException) {
+	Divider::printString($argumentException->getMessage());
+	$monolog->info($argumentException->getMessage());
+}
