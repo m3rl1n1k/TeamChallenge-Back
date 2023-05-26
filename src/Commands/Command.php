8ed66e6d963080ffe5e8d9c20514f1b3db77cb83
Bisix21 <119133287+Bisix21@users.noreply.github.com>
2023-05-26 22:22:36 +0200

    Update Command.php

--- src/Commands/Command.php
+++ src/Commands/Command.php
@@ -4,8 +4,14 @@ namespace Bisix21\src\Commands;
 
 abstract class Command
 {
+protected function getDataForArgumets()
+{
+  //передаємо дані для отримання аргументів  
+}
 	protected function getArgument($key)
 	{
-		return $this->arguments->getArguments()[$key];
+		return $this->arguments->getArguments(
+    $giveDataForArgument
+  );
 	}
 }
