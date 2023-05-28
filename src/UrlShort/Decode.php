278e37fdaabd362b9a65ece8d5f7d06f46978415
Fenix-25 <serqdrago@gmail.com>
2023-05-28 18:31:12 +0300

    добавлені методи для відповідних бд в декодері

--- src/UrlShort/Decode.php
+++ src/UrlShort/Decode.php
@@ -2,18 +2,34 @@
 
 namespace Bisix21\src\UrlShort;
 
+use Bisix21\src\UrlShort\Entity\Short;
 use Bisix21\src\UrlShort\Interface\IUrlDecoder;
+use Bisix21\src\UrlShort\Models\UrlShort;
+use Doctrine\ORM\EntityManager;
 
 
 class Decode implements IUrlDecoder
 {
 	public function __construct(
+		protected EntityManager|UrlShort $short
 	)
 	{
 	}
 
 	public function decode(string $code): string
 	{
-		return  "decode $code ";
+		return $this->decodeFromDM($code);
+	}
+	protected function decodeFromDM(string $code)
+	{
+		$shortRep = $this->short->getRepository(Short::class);
+		$short = $shortRep->getUrlByCode($code);
+		return $short->getUrl();
+	}
+
+	protected function decodeFromAR(string $code)
+	{
+		$res = $this->short->getUrlByCode($code);
+		return $res->url;
 	}
 }
