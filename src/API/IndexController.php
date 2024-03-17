f34d77c97bcfcdd27554419d1daf137fbe18d266
Serhii <bisix21@gmail.com>
2024-03-17 13:55:56 +0100

    make start

--- src/API/IndexController.php
+++ src/API/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\API;
 
 use App\Core\AbstractController;
 
--- src/route.php
+++ src/route.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Controller\IndexController;
+use App\API\IndexController;
 use App\Core\Request;
 use App\Core\Route;
 
