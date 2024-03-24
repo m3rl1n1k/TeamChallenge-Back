5c430b9bc36b08967266d4f88cb82723cbad6002
Serhii <bisix21@gmail.com>
2024-03-24 14:55:00 +0100

    fixing route '/product/show/{id}' with dynamic id when call '/home'

--- config/route.php
+++ config/route.php
@@ -6,7 +6,7 @@ use App\Core\Route;
 
 $route = new Route();
 
-$route->get('/', IndexController::class, 'index');
+$route->get('/home', IndexController::class, 'index');
 $route->get('/product/show/{show}', IndexController::class, 'test');
 
 $route->route(Request::getUrl(), $_POST['_method'] ?? Request::getMethod());
