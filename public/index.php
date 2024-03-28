c4ddc8afe1782de9cbae22dad534c3b49df1f0e9
Bisix21 <119133287+Bisix21@users.noreply.github.com>
2024-03-27 20:26:39 -0700

    future editions

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ error_reporting(E_ERROR);
 const ROOT = __DIR__ . "/../";
 require_once ROOT . 'src/bootstrap.php';
 try {
+// витягую екземпляр класу Rout з контейнера і викликаю в нього метод configRout().Додати даний метод до інтерфейсу класу
     require_once ROOT . 'config/route.php';
 } catch (Exception|ContainerExceptionInterface|Error $e) {
     echo $e->getMessage() . " " . $e->getLine() . " " . $e->getFile() . " " . "</b>";
